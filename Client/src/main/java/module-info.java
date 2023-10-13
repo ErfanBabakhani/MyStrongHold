2033c30a713413e69c6211f665f52623ab5f97c8
Erfan.B <iran166erf@gmail.com>
2023-10-13 18:03:58 +0330

    My strongHold

--- Client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module Tamrin {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-    exports view;
-    exports controller;
-    opens view to javafx.fxml;
-}
